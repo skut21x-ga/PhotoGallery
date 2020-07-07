cc88925191dd3a145632d24268550a2ee5b465e5
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-07-07 01:27:00 -0400

    set props into each figure

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -3,7 +3,23 @@ import React, { Component } from "react";
 class Photo extends Component {
   render() {
     const post = this.props.post;
-    return <figure className="figure"></figure>;
+    return (
+      <figure className="figure">
+        <div className="photo-container">
+          <img
+            className="photo"
+            src={post.imageLink}
+            alt={post.description}
+          ></img>
+        </div>
+        <figcaption>
+          <p>{post.description}</p>
+        </figcaption>
+        <div className="remove-button-container">
+          <button className="remove-button">Remove Photo</button>
+        </div>
+      </figure>
+    );
   }
 }
 
