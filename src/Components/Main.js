fd3e0b0e80ed2968527ad851c533e9708d096e34
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-07-07 00:33:51 -0400

    setup Title component

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Title from "./Title";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Title title={"Photo Gallery"}></Title>
+      </div>
+    );
+  }
+}
+export default Main;
