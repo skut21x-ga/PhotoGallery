73f693dbc42a9308da002b622c85e7c55abac68d
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-07-12 03:00:55 -0400

    added submit function for addphoto page

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 
 class AddPhoto extends Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("New Photo Submitted from:");
+    const imageURL = event.target.elements.URL.value;
+    const description = event.target.elements.Caption.value;
+  }
+
   render() {
     return (
       <div>
         <h1>Photo Gallery</h1>
         <div className="add-form">
-          <form>
-            <input type="text" placeholder="Photo URL" />
-            <input type="text" placeholder="Caption/Description" />
-            <button> post </button>
+          <form onSubmit={this.handleSubmit}>
+            <input name="URL" type="text" placeholder="Photo URL" />
+            <input
+              name="Caption"
+              type="text"
+              placeholder="Caption/Description"
+            />
+            <button> Post </button>
           </form>
         </div>
       </div>
